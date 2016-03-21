5bbd30ed93f4a089c7490f717f700c314d9db657
Jeff Hostetler <hostetjm@gmail.com>
2016-03-20 18:19:47 -0600

    You can add yourself to the list of users.

--- spec/controllers/user_controller_spec.exs
+++ spec/controllers/user_controller_spec.exs
@@ -20,4 +20,22 @@ defmodule AnyoneImportant.UserControllerSpec do
     it do: should render_template("index.html")
     it do: should have_in_assigns(all_users: all_users)
   end
+
+  describe "create" do
+    subject do: action(:create, %{"user" => %{"name" => "My Guy", "email" => "test@example.com"}})
+
+    it do: should have_in_flash info: "You are now on the list."
+    it do: should be_redirection
+
+
+    it "creates the user with valid params" do
+       action(:create, %{"user" => %{"name" => "My Guy", "email" => "test@example.com"}})
+
+       expect(Enum.count(Repo.all(User))).to eq 1
+
+       created_user = Repo.get_by(User, name: "My Guy")
+
+       expect(created_user.email).to eq "test@example.com"
+    end
+  end
 end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,8 +3,23 @@ defmodule AnyoneImportant.UserController do
   alias AnyoneImportant.User
   alias AnyoneImportant.Repo
 
+  plug :scrub_params, "user" when action in [:create, :update]
+
   def index(conn, _params) do
-    render conn, "index.html", all_users: Repo.all(User)
+    conn
+    |> put_flash(:info, "You are now on the list.")
+    |> assign(:all_users, Repo.all(User))
+    |> assign(:new_user, User.changeset(%User{}))
+    |> render("index.html")
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    %User{name: user_params["name"], email: user_params["email"]}
+    |>Repo.insert!
+
+    conn
+    |> put_flash(:info, "You are now on the list.")
+    |> redirect(to: user_path(conn, :index))
   end
 
 end
