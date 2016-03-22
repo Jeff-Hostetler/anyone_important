ad604687cc8c1e377dead9b43fc7ec69d6839d68
Jeff Hostetler <hostetjm@gmail.com>
2016-03-21 21:39:57 -0600

    Cleanup twitter search

--- spec/services/twitter_service_spec.exs
+++ spec/services/twitter_service_spec.exs
@@ -5,7 +5,7 @@ defmodule AnyoneImportant.TwitterServiceSpec do
   context "twitter specs", context_tag: :twitter do
     describe "#search" do
       it "returns a list of tweet based on search term" do
-        returned_list = TwitterService.search("@KanyeWest", "kanye")
+        returned_list = TwitterService.search("kanye")
 
         expect(Enum.count(returned_list)).to be_between(0,10)
 
--- web/services/twitter_service.ex
+++ web/services/twitter_service.ex
@@ -1,14 +1,9 @@
 defmodule AnyoneImportant.TwitterService do
 
-  def search(twitter_handle, search_term) do
+  def search(search_term) do
     search_result = ExTwitter.search(search_term, [count: 10]) |>
        Enum.map(fn(tweet) -> tweet.text end)
 
     Enum.to_list(search_result)
   end
 end
-
-search_result = ExTwitter.search("kanye", [count: 10]) |>
-       Enum.map(fn(tweet) -> tweet.text end)
-
-    Enum.to_list(search_result)
