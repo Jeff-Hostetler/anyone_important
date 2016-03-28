9962de9d55cc9e7bc6ae457cd5065791641e1dcf
Jeff Hostetler <hostetjm@gmail.com>
2016-03-27 22:05:18 -0600

    Include prod.secret.exs and use environment variables

---
+++ config/prod.secret.exs
@@ -0,0 +1,27 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :anyone_important, AnyoneImportant.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+
+# Configure your database
+config :anyone_important, AnyoneImportant.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASSWORD"),
+  database: "anyone_important_prod",
+  pool_size: 20
+
+config :extwitter, :oauth, [
+   consumer_key: System.get_env("CONSUMER_KEY"),
+   consumer_secret: System.get_env("CONSUMER_SECRET"),
+   access_token: System.get_env("ACCESS_TOKEN"),
+   access_token_secret: System.get_env("ACCESS_TOKEN_SECRET")
+]
+
+config :anyone_important,
+   mailgun_domain: System.get_env("MAILGUN_DOMAIN"),
+   mailgun_key: System.get_env("MAILGUN_KEY")
