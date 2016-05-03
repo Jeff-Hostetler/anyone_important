364dd8c3746e5fa0697e8e33220f7364893305fd
Jeff Hostetler <hostetjm@gmail.com>
2016-05-02 21:12:55 -0600

    Refactor specs

--- spec/controllers/user_controller_spec.exs
+++ spec/controllers/user_controller_spec.exs
@@ -9,7 +9,6 @@ defmodule AnyoneImportant.UserControllerSpec do
     end
 
     subject do: action :index
-
     it do: should be_successful
     it do: should have_http_status(:ok)
     it do: should render_template("index.html")
@@ -17,18 +16,19 @@ defmodule AnyoneImportant.UserControllerSpec do
   end
 
   describe "create" do
-    subject do: action(:create, %{"user" => %{"name" => "My Guy", "email" => "test@example.com"}})
-    it do: should have_in_flash info: "You are now on the list."
-    it do: should be_redirection
-
     it "creates the user with valid params" do
-       action(:create, %{"user" => %{"name" => "My Guy", "email" => "test@example.com"}})
+      create_action = action(
+        :create,
+        %{"user" => %{"name" => "My Guy", "email" => "test@example.com"}}
+      )
 
-       expect(Enum.count(Repo.all(User))).to eq 1
+      expect create_action |> to(have_in_flash(info: "You are now on the list."))
+      expect create_action |> to(be_redirection)
+      expect(Enum.count(Repo.all(User))).to eq 1
 
-       created_user = Repo.get_by(User, name: "My Guy")
+      created_user = Repo.get_by(User, name: "My Guy")
 
-       expect(created_user.email).to eq "test@example.com"
+      expect(created_user.email).to eq "test@example.com"
     end
   end
 end
